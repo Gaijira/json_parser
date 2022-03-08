bf01012ce4efb029edbfeb28010b310de0193279
Alex Avdeev <gajira@MacBook-Pro-Alex.local>
2022-03-08 21:33:15 +0300

    исправлена форматирующая функция

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import json
+from typing import Any
 import yaml
 
 
-def formatter(item, key, value) -> None:
+def data_formatter(item: Any, key: Any, value: Any) -> None:
     """Форматирует значения ключей в зависимости от типа данных, ключей и значений"""
     if type(value) == int and key != 'id':
         item[key] = "positive()"
@@ -12,9 +13,9 @@ def formatter(item, key, value) -> None:
         item[key] = 'empty()'
     elif key == 'created_at' or key == 'updated_at' or key == 'start_at' or key == 'expiration_at':
         item[key] = "date('RFC3339')"
-    elif key == 'False':
+    elif value == 'False':
         item[key] = 'eq(false)'
-    elif key == 'True':
+    elif value == 'True':
         item[key] = 'eq(true)'
     else:
         item[key] = f'eq("{value}")'
@@ -29,26 +30,26 @@ def data_maker() -> dict:
         for item in data:
             for key, value in item.items():
                 if not isinstance(value ,dict) and not isinstance(value, list):
-                    formatter(item, key, value)
+                    data_formatter(item, key, value)
                 elif isinstance(value, dict):
                     for inner_key, inner_value in value.items():
-                        formatter(value, inner_key, inner_value)
+                        data_formatter(value, inner_key, inner_value)
                 elif isinstance(value, list):
                     if any(isinstance(elem, dict) for elem in value):
                         for el in value:
                             if isinstance(el, dict):
                                 for inner_key, inner_value in el.items():
                                     if not isinstance(inner_value, dict):
-                                        formatter(el, inner_key, inner_value)
+                                        data_formatter(el, inner_key, inner_value)
                                     elif isinstance(inner_value, dict):
                                         for nested_key, nested_value in inner_value.items():
-                                            formatter(inner_value, nested_key, nested_value)
+                                            data_formatter(inner_value, nested_key, nested_value)
                     elif all(not isinstance(elem, dict) for elem in value):
-                        formatter(item, key, value)
+                        data_formatter(item, key, value)
     return data
 
 
-def yaml_data_writer(data):
+def yaml_data_writer(data: dict) -> None:
     """Записывает переданный словарь в yaml формате в parsed_data.yaml"""
     with open('parsed_data.yml', 'w') as f2:
         yaml.dump(data, f2, encoding='UTF-8', allow_unicode=True, sort_keys=False)
