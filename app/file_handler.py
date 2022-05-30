7c5feadefdf97f65c07a3b584a37e112c5ffcb91
Alexandr <44476785+Gaijira@users.noreply.github.com>
2022-05-30 17:59:00 +0300

    Update file_handler.py

--- app/file_handler.py
+++ app/file_handler.py
@@ -6,15 +6,15 @@ import yaml
 
 class FileHandler():
     
-    root_dir = os.path.dirname(os.path.abspath(__file__))
+    self.root_dir = os.path.dirname(os.path.abspath(__file__))
 
     def yaml_data_writer(data: dict) -> None:
         """Записывает переданный словарь в yaml формате в parsed_data.yaml"""
-        with open(os.path.join(root_dir, 'parsed_data.yml'), 'w') as f2:
+        with open(os.path.join(self.root_dir, 'parsed_data.yml'), 'w') as f2:
             yaml.dump(data, f2, encoding='UTF-8', allow_unicode=True, sort_keys=False)
 
     def json_data_loader() -> dict:
         """Выгружает данные из файла data.json и возвращает из в формате словаря"""
-        with open(os.path.join(root_dir, 'data.json'), 'r') as f:
+        with open(os.path.join(self.root_dir, 'data.json'), 'r') as f:
             data = json.load(f)
         return data
